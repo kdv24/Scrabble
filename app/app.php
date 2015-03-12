ede9f80054cc4290d9399433cd2c61888e47c6ed
Kelly de Vries & David Eliason <student@epicodus.com>
2015-03-12 13:45:01 -0700

    Makes routes correct in index.php to app.php.

--- app/app.php
+++ app/app.php
@@ -4,8 +4,15 @@
     require_once __DIR__.'/../src/Scrabble.php';
 
     $app = new Silex\Application();
+    $app['debug']=true;
 
     $app->register(new Silex\Provider\TwigServiceProvider, array('twig.path' => __DIR__.'/../views'));
 
+    $app->get("/", function() use ($app)
+    {
+            return $app['twig'] -> render('home.twig');
+    });
+
+return $app;
 
 ?>
