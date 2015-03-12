a2e5a70f16684afe98b808f6bd2e6eca9c2774e7
Kelly de Vries & David Eliason <student@epicodus.com>
2015-03-12 15:29:38 -0700

    Removes erroneous return score in class method.  Also creates output variable to catch input from instantiated class and calls method.  Adds return score at end of method, after the loop.

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -5,44 +5,63 @@
         {   $score = 0;
             $input_characters = array();
             $input_characters = str_split($player_input);
+            // var_dump($input_characters);
             foreach ($input_characters as $letter)
             {
                 //A E I O U L N R S T
                 if(($letter == "A") || ($letter == "E") || ($letter == "I") || ($letter == "O") || ($letter == "U") || ($letter == "L") || ($letter == "N") || ($letter == "R") || ($letter == "S") || ($letter == "T"))
                 {
                 $score = $score + 1;
+                
                 }
 
                 elseif(($letter == "G") || ($letter =="D"))
                 {
                 $score = $score +  2;
+                // var_dump($score);
+                
+
                 }
 
                 elseif(($letter =="B") || ($letter =="C") || ($letter == "M")  || ($letter =="P"))
                 {
                 $score = $score +  3;
+                
+
                 }
 
                 elseif(($letter == "F") || ($letter == "H") || ($letter =="V") || ($letter == "W") || ($letter == "Y"))
                 {
                 $score = $score +  4;
+                
+
                 }
 
                 elseif($letter == "K")
                 {
                 $score = $score +  5;
+                
+
                 }
 
                 elseif(($letter == "J") || ($letter == "X"))
                 {
                 $score = $score +  8;
+                
+
                 }
 
                 elseif(($letter == "Q") || ($letter == "Z"))
                 {
                 $score = $score +  10;
+                
                 }
+
             }
+            // var_dump($score);
+
+            return $score;
+
         }
 
 
