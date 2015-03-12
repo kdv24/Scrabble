f80521ae62bcddfb42ffb00f71948f5769ac69e6
Kelly de Vries & David Eliason <student@epicodus.com>
2015-03-12 15:32:41 -0700

    Adds return button on results page

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -4,7 +4,7 @@
         function getWordValue($player_input)
         {   $score = 0;
             $input_characters = array();
-            $input_characters = str_split($player_input);
+            $input_characters = str_split(ucwords($player_input));
             // var_dump($input_characters);
             foreach ($input_characters as $letter)
             {
@@ -12,49 +12,49 @@
                 if(($letter == "A") || ($letter == "E") || ($letter == "I") || ($letter == "O") || ($letter == "U") || ($letter == "L") || ($letter == "N") || ($letter == "R") || ($letter == "S") || ($letter == "T"))
                 {
                 $score = $score + 1;
-                
+
                 }
 
                 elseif(($letter == "G") || ($letter =="D"))
                 {
                 $score = $score +  2;
                 // var_dump($score);
-                
+
 
                 }
 
                 elseif(($letter =="B") || ($letter =="C") || ($letter == "M")  || ($letter =="P"))
                 {
                 $score = $score +  3;
-                
+
 
                 }
 
                 elseif(($letter == "F") || ($letter == "H") || ($letter =="V") || ($letter == "W") || ($letter == "Y"))
                 {
                 $score = $score +  4;
-                
+
 
                 }
 
                 elseif($letter == "K")
                 {
                 $score = $score +  5;
-                
+
 
                 }
 
                 elseif(($letter == "J") || ($letter == "X"))
                 {
                 $score = $score +  8;
-                
+
 
                 }
 
                 elseif(($letter == "Q") || ($letter == "Z"))
                 {
                 $score = $score +  10;
-                
+
                 }
 
             }
